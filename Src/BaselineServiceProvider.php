73688e1420cde855e6a7ee5284264cb14d6e324b
Jake Suellentrop <jake@lifeboatcreative.com>
2021-04-17 09:51:01 -0500

    adds service provider

---
+++ Src/BaselineServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Baseline;
+
+use Illuminate\Support\ServiceProvider;
+
+class BaselineServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
